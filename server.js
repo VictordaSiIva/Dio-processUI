d43b691dcf50877833cf04cb0bd2adb70625b5a8
Victor da Silva <Victor10019@hotmail.com>
2020-07-03 20:20:56 -0300

    deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/src/'));
+app.use(express.static(__dirname + '/dist/Processos'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/src/Processos/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
